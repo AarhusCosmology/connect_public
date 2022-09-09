b0007fa8e1b2c5a1bcac2338a17d262a359f2f68
Andreas Nygaard <andreas@phys.au.dk>
2022-09-09 13:43:21 +0200

    7 mac compatibility (#8)
    * Fixed bug in connect.py (#7)
    
    * 8 fix mpi bug (#9)
    
    * Fixed bug in connect.py (#3)
    
    * Fixed compatibility with older CLASS
    
    * Minor fix
    
    * Added system-check

--- source/shell_scripts/number_of_tasks.sh
+++ source/shell_scripts/number_of_tasks.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+run_env="$(uname)"
+
 if [ -z $SLURM_NPROCS ]
 then
-     N_tasks=$(nproc)
-     N_cpus_per_task="1"
+    N_tasks=$(nproc)
+    N_cpus_per_task="1"
+    if [[ $run_env =~ "Darwin" ]]
+    then
+ 	N_tasks=$(sysctl -n hw.logicalcpu)
+    else
+ 	N_tasks=$(nproc)
+    fi
 else
-     N_tasks=$SLURM_NPROCS
-     N_cpus_per_task=$SLURM_CPUS_PER_TASK
+    N_tasks=$SLURM_NPROCS
+    N_cpus_per_task=$SLURM_CPUS_PER_TASK
 fi
 
 echo "${N_tasks},${N_cpus_per_task}"
