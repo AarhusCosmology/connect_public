35bb5ee7f0077efe9cb1e248424dad20bcc93ac0
Andreas Nygaard <andreas@phys.au.dk>
2023-05-02 14:44:40 +0200

    Improved training accuracy (#30)
    * Added MultiNest and PolyChord to setup.sh
    
    * Fixed small mistakes in setup.sh
    
    * Added batch normalisation, Glorot initialisation, drop-out, and new callbacks
    
    * Added customisability
    
    * Updated version number

--- source/architecture/sequential_models.py
+++ source/architecture/sequential_models.py
@@ -10,7 +10,12 @@ class Dense_model(tf.keras.Model):
                  activation='alsing',
                  normalizer = 0,
                  num_hidden_layers = 4,
+                 dropout = True,
+                 batch_norm = True,
                  output_info = 0):
+
+        self.dropout_bool = dropout
+        self.batch_norm_bool = batch_norm
 
         # Inherit from tf.keras.Model
         super(Dense_model, self).__init__()
@@ -19,6 +24,8 @@ class Dense_model(tf.keras.Model):
         self.normalizer = normalizer
         self.num_hidden_layers = num_hidden_layers
         self.hidden_layers = []
+        self.batch_norm_layers = []
+        self.dropout_layers = []
 
 
         ### Define architecture ###
@@ -39,9 +46,12 @@ class Dense_model(tf.keras.Model):
             else:
                 act_fun = activation
             # Create hidden layer
-            self.hidden_layers.append(tf.keras.layers.Dense(N_nodes, activation=act_fun))
+            self.hidden_layers.append(tf.keras.layers.Dense(N_nodes, activation=act_fun, kernel_initializer='glorot_normal', bias_initializer='glorot_normal', kernel_constraint=tf.keras.constraints.MaxNorm(5)))
+            self.dropout_layers.append(tf.keras.layers.Dropout(0.2))
+            if i%5 == 4:
+                self.batch_norm_layers.append(tf.keras.layers.BatchNormalization())
         # Create output layer
-        self.output_layer = tf.keras.layers.Dense(num_out, activation='linear')
+        self.output_layer = tf.keras.layers.Dense(num_out, activation='linear', kernel_initializer='glorot_normal', bias_initializer='glorot_normal')
 
     def call(self, x, **kwargs):
         if self.normalizer:
@@ -49,5 +59,9 @@ class Dense_model(tf.keras.Model):
         x = self.input_layer(x)
         for i in range(self.num_hidden_layers):
             x = self.hidden_layers[i](x)
+            if self.dropout_bool:
+                x = self.dropout_layers[i](x)
+            if i%5 == 4 and self.batch_norm_bool:
+                x = self.batch_norm_layers[int(i/5)](x)
         x = self.output_layer(x)
         return x
