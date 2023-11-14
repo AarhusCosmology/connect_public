3ad1dce585e9e0d6facd1dc744a786ffcd93fd38
Andreas Nygaard <andreas@phys.au.dk>
2023-11-14 08:26:39 +0100

    Fixed error handling in default_module.py (#43)

--- source/default_module.py
+++ source/default_module.py
@@ -57,7 +57,6 @@ class Parameters():
         
         for key, val in default.items():
             setattr(self, key, getattr(param, key, val[0]))
-            print(eval(f'self.{key}'))
         self.error_handling(param, default)
 
 
@@ -72,67 +71,64 @@ class Parameters():
             return k_grid
         return None
 
-    def cast_to_native_type(self, value, target_type, boolean=False):
-        if boolean:
-            try:
-                cast_to_native_type(value, target_type)
-                return True
-            except:
-                return False
-        return eval(f"{target_type.__name__}(value)")
-    
+    def try_cast_to_native_type(self, value, target_type):
+        try:
+            return target_type(value)
+        except:
+            pass
+
     def error_handling(self, param, default):
+        sep = '\n' + '    '*2 + '- '
         name_errors = self.get_name_errors(param, default)
         type_errors = self.get_type_errors(param, default)
         input_error = 'Error with inputs in parameter file'
-        if len(type_errors) > 11 and len(name_errors) > 11:
-            input_errors = input_error + '\n' + name_errors + '\n' + type_errors
+        if len(type_errors) > 1 and len(name_errors) > 1:
+            input_errors = input_error + '\n' + sep.join(name_errors) + '\n' + sep.join(type_errors)
             raise Exception(input_errors)
-        elif len(name_errors) > 11:
-            name_errors = input_error + '\n' + name_errors
+        elif len(name_errors) > 1:
+            name_errors = input_error + '\n' + sep.join(name_errors)
             raise Exception(name_errors)
-        elif len(type_errors) > 11:
-            type_errors = input_error + '\n' + type_errors
+        elif len(type_errors) > 1:
+            type_errors = input_error + '\n' + sep.join(type_errors)
             raise Exception(type_errors)
         if not self.parameters:
             raise ValueError('No set of parameters were given. Please provide a dictionary with lower and upper bounds.')
 
     def get_name_errors(self, param, default):
-        name_errors = 'NameErrors:'
+        name_errors = ['NameErrors:']
         for name in [n for n in dir(param) if not n.startswith('__') and not n.endswith('__')]:
             if not name in default:
                 matches = dl.get_close_matches(name, default.keys())
                 if len(matches) == 0:
-                    name_errors += '\n' + '    '*2 + '- '
-                    name_errors += f"'{name}' is not a recognised parameter and no close matches exist."
+                    name_error = f"'{name}' is not a recognised parameter and no close matches exist."
                 else:
-                    name_errors += '\n' + '    '*2 + '- '
-                    name_errors += f"'{name}' is not a recognised parameter. Did you mean '{matches[0]}'?"
+                    name_error = f"'{name}' is not a recognised parameter. Did you mean '{matches[0]}'?"
                     input_val = getattr(param, name)
                     input_type = type(input_val)
                     target_type = default[matches[0]][1]
                     if input_type != target_type:
                         if target_type == bool:
-                            name_errors += f" The type should then be 'bool'."
-                        elif (not self.cast_to_native_type(input_val, target_type, boolean=True)
+                            name_error += f" The type should then be 'bool'."
+                        elif (self.try_cast_to_native_type(input_val, target_type) == None
                               or (target_type in [list, tuple, set] and input_type == str)):
-                            name_errors += f" The type should then be '{target_type.__name__}'."
+                            name_error += f" The type should then be '{target_type.__name__}'."
+                name_errors.append(name_error)
         return name_errors
 
     def get_type_errors(self, param, default):
-        type_errors = 'TypeErrors:'
+        type_errors = ['TypeErrors:']
         for key, val in default.items():
             input_val = getattr(self, key)
             input_type = type(input_val)
+            target_val = val[0]
             target_type = val[1]
-            if key in dir(param) and type(input_val) != val[1]:
+            if hasattr(param, key) and input_type != target_type and target_val != None:
                 if target_type == bool:
-                    type_errors += '\n' + '    '*2 + '- '
-                    type_errors += f"'{key}' is of type '{input_type.__name__}' and should be of type 'bool'."
-                elif (not self.cast_to_native_type(input_val, target_type, boolean=True)
-                      or (target_type in [list, tuple, set] and input_type == str)):
-                    type_errors += '\n' + '    '*2 + '- '
-                    type_errors += f"'{key}' is of type '{input_type.__name__}' and could not be converted to type '{target_type.__name__}'."
+                    type_errors.append(f"'{key}' is of type '{input_type.__name__}' and should be of type 'bool'.")
+                    continue
+                casting = self.try_cast_to_native_type(input_val, target_type)
+                if casting == None or (target_type in [list, tuple, set] and input_type == str):
+                    type_errors.append(f"'{key}' is of type '{input_type.__name__}' and could not be converted to type '{target_type.__name__}'.")
                 else:
-                    setattr(self, key, cast_to_native_type(input_val, target_type))
+                    setattr(self, key, casting)
         return type_errors
